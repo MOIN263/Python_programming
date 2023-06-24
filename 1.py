0b0646da8f560bc97c21e45d694b3cf1d09015f4
MOIN263 <smoinullah1@gmail.com>
2023-06-24 16:25:44 +0530

    first python file

---
+++ 1.py
@@ -0,0 +1,15 @@
+# basic python program 
+
+# printing hello world 
+
+print('hello world')
+
+# taking an input an printing it
+
+a= input('enter anything to print:')
+print(a)
+
+# printing hello world using a variable
+
+msg = 'hello world'
+print(msg)
