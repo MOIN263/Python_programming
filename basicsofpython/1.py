edf8b8e0cf7ed32e747d7d86d575c2348b0c9c9a
MOIN263 <smoinullah1@gmail.com>
2023-07-26 21:40:38 +0530

    basics

---
+++ basicsofpython/1.py
@@ -0,0 +1,21 @@
+
+# print statements 
+# print is an inbuilt function
+# syntax print()
+print(7)
+
+# print strings 
+# strings are denoted with double quotes aur single Quotes 
+print('hello world')
+print("hello Smuf")
+
+# variables 
+
+age = 22 
+first_name = 'SMUF'
+boolean = True
+boolean =False
+
+print(age)
+print(first_name)
+print(boolean)
