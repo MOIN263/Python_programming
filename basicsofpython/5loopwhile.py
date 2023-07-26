cb51ed08770c44a96efb57b78f126c77d9dae240
MOIN263 <smoinullah1@gmail.com>
2023-07-26 21:44:17 +0530

    basics

---
+++ basicsofpython/5loopwhile.py
@@ -0,0 +1,24 @@
+# While loop
+
+# syntax 
+
+#  i = 0 /initialization
+# while (condition for termination) :
+      # block of code 
+    #   i+=1/i-=1 incrementation /decrementation respectively
+    
+    
+# write a code with while loop to print the number from 1 to 10
+
+i=1
+while i<=10:
+    print(i)
+    i+=1
+    
+# write a code to print the sum of n numbers 
+sum=0
+i=1
+while i<=10:
+  sum+=i
+  print(sum)
+  i+=1
